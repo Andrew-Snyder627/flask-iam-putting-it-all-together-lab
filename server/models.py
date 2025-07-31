6419da28d3aeb832992ff360403d259373b07935
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-07-31 19:02:33 -0400

    User Model in place with bcrypt hash logic

--- server/models.py
+++ server/models.py
@@ -4,18 +4,48 @@ from marshmallow import Schema, fields
 
 from config import db, bcrypt
 
+
 class User(db.Model):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship(
+        'Recipe', back_populates='user', cascade='all, delete-orphan')
+
+    # Password Hash logic with bcrypt
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')).decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username must be present")
+        return username
+
 
 class Recipe(db.Model):
     __tablename__ = 'recipes'
-    
+
     pass
+
 
 class UserSchema(Schema):
     pass
+
 
 class RecipeSchema(Schema):
     pass
